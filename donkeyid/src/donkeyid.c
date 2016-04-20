b6677a790c3141b444cdac1e1c2f2d57f684c56b
ClownFish <osgochina@gmail.com>
2016-04-20 18:35:29 +0800

    添加测试

--- donkeyid/src/donkeyid.c
+++ donkeyid/src/donkeyid.c
@@ -248,26 +248,3 @@ __uint64_t donkeyid_next_id() {
     spin_unlock(&((lock+dtype)->lock), pid);
     return id;
 }
-
-
-//int main() {
-//    int type = 1;
-//    long epoch = 1460710318;
-//    donkeyid_init(0);
-//    donkeyid_set_worker_id();
-//    donkeyid_set_type(type);
-//    donkeyid_set_node_id(99);
-//    donkeyid_set_epoch(epoch);
-//
-//    int i = 0;
-//    for (i = 0; i < 1; ++i) {
-//        __uint64_t donkeyid = donkeyid_next_id();
-//        printf("%"PRIu64"\n", donkeyid);
-//        printf("%"PRIu64"\n",GET_TIMESTAMP_BY_DONKEY_ID(donkeyid,type,epoch));
-//        printf("%d\n",GET_NODE_ID_BY_DONKEY_ID(donkeyid,type));
-//        printf("%d\n",GET_WORKER_ID_BY_DONKEY_ID(donkeyid,type));
-//        printf("%d\n",GET_SEQUENCE_BY_DONKEY_ID(donkeyid,type));
-//    }
-//    donkeyid_shutdown(1);
-//}
-
