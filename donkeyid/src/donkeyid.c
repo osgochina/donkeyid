880f2de2ee27cebe4098f297b35be72e7bf1c1ab
ClownFish <osgochina@gmail.com>
2017-04-20 14:50:48 +0800

    fixbug

--- donkeyid/src/donkeyid.c
+++ donkeyid/src/donkeyid.c
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-#include <sys/malloc.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <string.h>
